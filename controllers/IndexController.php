7858e999887b3086ecdff14a4f23ba56eda46541
padamczyk2 <piotr.adamczyk@estoremedia.com>
2022-02-20 12:31:32 +0100

    update 20.02

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -27,12 +27,16 @@ class IndexController
     {
         if (isset($data['delete'])) {
             CustomersRepository::getInstance()->deleteByIds($data['users']);
+            header('Location: /index');
+        } elseif(isset($data['add'])) {
+            header('Location: /user');
         } elseif (isset($data['save'])) {
             $customer = new Customer();
             $customer->setFirstName($data["first_name"]);
             $customer->setLastName($data["last_name"]);
+            $customer->setEmail($data["email"]);
+            $customer->setPhone($data["phone"]);
             CustomersRepository::getInstance()->createUser($customer);
-
             header('Location: /index');
         }
     }
